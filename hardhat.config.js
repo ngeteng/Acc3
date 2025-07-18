26a5401fbce55674879d2c95051d8cf4b85cfb0a
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-07-18 19:01:11 +0700

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,21 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { PRIVATE_KEY, RPC_URL_0G } = process.env;
+
+if (!PRIVATE_KEY || !RPC_URL_0G) {
+  console.error("Harap set PRIVATE_KEY dan RPC_URL_0G di dalam file .env");
+  process.exit(1);
+}
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    og_testnet: {
+      url: RPC_URL_0G,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  defaultNetwork: "hardhat",
+};
