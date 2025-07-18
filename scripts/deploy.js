fbe892439e7fa60a9ae02b1a8680df1401e6d73a
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-07-18 19:34:04 +0700

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,11 +2,11 @@ const { ethers } = require("hardhat");
 
 async function main() {
   console.log("Mendeploy kontrak DomainService...");
-
+  
   const DomainService = await ethers.getContractFactory("DomainService");
   const domainService = await DomainService.deploy();
   await domainService.waitForDeployment();
-
+  
   const contractAddress = await domainService.getAddress();
   console.log(`✅ Kontrak berhasil di-deploy di alamat: ${contractAddress}`);
 }
